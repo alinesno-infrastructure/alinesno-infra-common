e7a7998e1077189b7e5f1c710ec82df4d0006413
luoxiaodong <luoandon@gmail.com>
2024-02-10 17:42:12 +0800

    feat:集成mybatis-plus加密

--- alinesno-infra-common-web/alinesno-infra-common-web-api/src/main/java/com/alinesno/infra/common/web/adapter/exception/GlobalExceptionHandler.java
+++ alinesno-infra-common-web/alinesno-infra-common-web-api/src/main/java/com/alinesno/infra/common/web/adapter/exception/GlobalExceptionHandler.java
@@ -1,14 +1,20 @@
 package com.alinesno.infra.common.web.adapter.exception;
 
+import com.alinesno.infra.common.core.exception.DemoModeException;
+import com.alinesno.infra.common.core.exception.ServiceException;
+import com.alinesno.infra.common.core.exception.base.BaseException;
+import com.alinesno.infra.common.core.utils.StringUtils;
 import com.alinesno.infra.common.facade.response.AjaxResult;
 import com.alinesno.infra.common.facade.response.HttpStatus;
 import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.apache.ibatis.exceptions.PersistenceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.validation.BindException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -56,26 +62,6 @@ public class GlobalExceptionHandler {
 	}
 
 	/**
-	 * 自定义验证异常
-	 */
-	@ExceptionHandler(BindException.class)
-	public AjaxResult validatedBindException(BindException e) {
-		log.error(e.getMessage(), e);
-		String message = e.getAllErrors().get(0).getDefaultMessage();
-		return AjaxResult.error(message);
-	}
-
-	/**
-	 * 自定义验证异常
-	 */
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public Object validExceptionHandler(MethodArgumentNotValidException e) {
-		log.error(e.getMessage(), e);
-		String message = e.getBindingResult().getFieldError().getDefaultMessage();
-		return AjaxResult.error(message);
-	}
-
-	/**
 	 * 持久层异常处理
 	 *
 	 * @param e
@@ -87,4 +73,79 @@ public class GlobalExceptionHandler {
 		return AjaxResult.error("持久层错误，请稍后联系管理员");
 	}
 
+	/**
+	 * 请求方式不支持
+	 */
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	public AjaxResult handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException e,
+														  HttpServletRequest request)
+	{
+		String requestURI = request.getRequestURI();
+		log.error("请求地址'{}',不支持'{}'请求", requestURI, e.getMethod());
+		return AjaxResult.error(e.getMessage());
+	}
+
+	/**
+	 * 业务异常
+	 */
+	@ExceptionHandler(ServiceException.class)
+	public AjaxResult handleServiceException(ServiceException e, HttpServletRequest request)
+	{
+		log.error(e.getMessage(), e);
+		Integer code = e.getCode();
+		return StringUtils.isNotNull(code) ? AjaxResult.error(code, e.getMessage()) : AjaxResult.error(e.getMessage());
+	}
+
+	/**
+	 * 拦截未知的运行时异常
+	 */
+	@ExceptionHandler(RuntimeException.class)
+	public AjaxResult handleRuntimeException(RuntimeException e, HttpServletRequest request)
+	{
+		String requestURI = request.getRequestURI();
+		log.error("请求地址'{}',发生未知异常.", requestURI, e);
+		return AjaxResult.error(e.getMessage());
+	}
+
+	/**
+	 * 系统异常
+	 */
+	@ExceptionHandler(Exception.class)
+	public AjaxResult handleException(Exception e, HttpServletRequest request)
+	{
+		String requestURI = request.getRequestURI();
+		log.error("请求地址'{}',发生系统异常.", requestURI, e);
+		return AjaxResult.error(e.getMessage());
+	}
+
+	/**
+	 * 自定义验证异常
+	 */
+	@ExceptionHandler(BindException.class)
+	public AjaxResult handleBindException(BindException e)
+	{
+		log.error(e.getMessage(), e);
+		String message = e.getAllErrors().get(0).getDefaultMessage();
+		return AjaxResult.error(message);
+	}
+
+	/**
+	 * 自定义验证异常
+	 */
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Object handleMethodArgumentNotValidException(MethodArgumentNotValidException e)
+	{
+		log.error(e.getMessage(), e);
+		String message = e.getBindingResult().getFieldError().getDefaultMessage();
+		return AjaxResult.error(message);
+	}
+
+	/**
+	 * 演示模式异常
+	 */
+	@ExceptionHandler(DemoModeException.class)
+	public AjaxResult handleDemoModeException(DemoModeException e)
+	{
+		return AjaxResult.error("演示模式，不允许操作");
+	}
 }
--- alinesno-infra-common-web/alinesno-infra-common-web-api/src/main/java/com/alinesno/infra/common/web/adapter/exception/file/FileException.java
+++ alinesno-infra-common-web/alinesno-infra-common-web-api/src/main/java/com/alinesno/infra/common/web/adapter/exception/file/FileException.java
@@ -1,6 +1,8 @@
 package com.alinesno.infra.common.web.adapter.exception.file;
 
-import com.alinesno.infra.common.web.adapter.exception.BaseException;
+import com.alinesno.infra.common.core.exception.base.BaseException;
+
+import java.io.Serial;
 
 /**
  * 文件信息异常类
@@ -8,6 +10,7 @@ import com.alinesno.infra.common.web.adapter.exception.BaseException;
  * @author ruoyi
  */
 public class FileException extends BaseException {
+	@Serial
 	private static final long serialVersionUID = 1L;
 
 	public FileException(String code, Object[] args) {
