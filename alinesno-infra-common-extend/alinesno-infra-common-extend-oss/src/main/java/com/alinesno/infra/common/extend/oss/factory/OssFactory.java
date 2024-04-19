ac2c262d8bc8c91b355d4201a237daa1acdc624c
carrocean <12559203+radish-hi_0@user.noreply.gitee.com>
2024-04-19 11:26:22 +0800

    feat: 更新common包

---
+++ alinesno-infra-common-extend/alinesno-infra-common-extend-oss/src/main/java/com/alinesno/infra/common/extend/oss/factory/OssFactory.java
@@ -0,0 +1,64 @@
+package com.alinesno.infra.common.extend.oss.factory;
+
+import com.alinesno.infra.common.core.cache.RedisUtils;
+import com.alinesno.infra.common.core.utils.JsonUtils;
+import com.alinesno.infra.common.core.utils.StringUtils;
+import com.alinesno.infra.common.extend.oss.cache.OssCacheUtils;
+import com.alinesno.infra.common.extend.oss.constant.OssConstant;
+import com.alinesno.infra.common.extend.oss.core.OssClient;
+import com.alinesno.infra.common.extend.oss.exception.OssException;
+import com.alinesno.infra.common.extend.oss.properties.OssProperties;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 文件上传Factory
+ *
+ * @author Lion Li
+ */
+@Slf4j
+public class OssFactory {
+
+    private static final Map<String, OssClient> CLIENT_CACHE = new ConcurrentHashMap<>();
+
+    /**
+     * 获取默认实例
+     */
+    public static OssClient instance() {
+        // 获取redis 默认类型
+        String configKey = RedisUtils.getCacheObject(OssConstant.DEFAULT_CONFIG_KEY);
+        if (StringUtils.isEmpty(configKey)) {
+            throw new OssException("文件存储服务类型无法找到!");
+        }
+        return instance(configKey);
+    }
+
+    /**
+     * 根据类型获取实例
+     */
+    public static synchronized OssClient instance(String configKey) {
+        String json = OssCacheUtils.get(OssConstant.SYS_OSS_CONFIG, configKey);
+        if (json == null) {
+            throw new OssException("系统异常, '" + configKey + "'配置信息不存在!");
+        }
+        OssProperties properties = JsonUtils.parseObject(json, OssProperties.class);
+        // 使用租户标识避免多个租户相同key实例覆盖
+        String key = properties.getTenantId() + ":" + configKey;
+        OssClient client = CLIENT_CACHE.get(key);
+        if (client == null) {
+            CLIENT_CACHE.put(key, new OssClient(configKey, properties));
+            log.info("创建OSS实例 key => {}", configKey);
+            return CLIENT_CACHE.get(key);
+        }
+        // 配置不相同则重新构建
+        if (!client.checkPropertiesSame(properties)) {
+            CLIENT_CACHE.put(key, new OssClient(configKey, properties));
+            log.info("重载OSS实例 key => {}", configKey);
+            return CLIENT_CACHE.get(key);
+        }
+        return client;
+    }
+
+}
---
+++ alinesno-infra-common-extend/alinesno-infra-common-extend-websocket/src/main/java/com/alinesno/infra/common/extend/websocket/handler/PlusWebSocketHandler.java
@@ -0,0 +1,95 @@
+package com.alinesno.infra.common.extend.websocket.handler;
+
+import com.alinesno.infra.common.extend.websocket.utils.WebSocketUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.socket.*;
+import org.springframework.web.socket.handler.AbstractWebSocketHandler;
+
+/**
+ * WebSocketHandler 实现类
+ *
+ * @author zendwang
+ */
+@Slf4j
+public class PlusWebSocketHandler extends AbstractWebSocketHandler {
+
+    /**
+     * 连接成功后
+     */
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+//        LoginUser loginUser = (LoginUser) session.getAttributes().get(LOGIN_USER_KEY);
+//        WebSocketSessionHolder.addSession(loginUser.getUserId(), session);
+//        log.info("[connect] sessionId: {},userId:{},userType:{}", session.getId(), loginUser.getUserId(), loginUser.getUserType());
+    }
+
+    /**
+     * 处理发送来的文本消息
+     *
+     * @param session
+     * @param message
+     * @throws Exception
+     */
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+//        LoginUser loginUser = (LoginUser) session.getAttributes().get(LOGIN_USER_KEY);
+//        List<Long> userIds = List.of(loginUser.getUserId());
+//        WebSocketMessageDto webSocketMessageDto = new WebSocketMessageDto();
+//        webSocketMessageDto.setSessionKeys(userIds);
+//        webSocketMessageDto.setMessage(message.getPayload());
+//        WebSocketUtils.publishMessage(webSocketMessageDto);
+    }
+
+    @Override
+    protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws Exception {
+        super.handleBinaryMessage(session, message);
+    }
+
+    /**
+     * 心跳监测的回复
+     *
+     * @param session
+     * @param message
+     * @throws Exception
+     */
+    @Override
+    protected void handlePongMessage(WebSocketSession session, PongMessage message) throws Exception {
+        WebSocketUtils.sendPongMessage(session);
+    }
+
+    /**
+     * 连接出错时
+     *
+     * @param session
+     * @param exception
+     * @throws Exception
+     */
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        log.error("[transport error] sessionId: {} , exception:{}", session.getId(), exception.getMessage());
+    }
+
+    /**
+     * 连接关闭后
+     *
+     * @param session
+     * @param status
+     */
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+//        LoginUser loginUser = (LoginUser) session.getAttributes().get(LOGIN_USER_KEY);
+//        WebSocketSessionHolder.removeSession(loginUser.getUserId());
+//        log.info("[disconnect] sessionId: {},userId:{},userType:{}", session.getId(), loginUser.getUserId(), loginUser.getUserType());
+    }
+
+    /**
+     * 是否支持分片消息
+     *
+     * @return
+     */
+    @Override
+    public boolean supportsPartialMessages() {
+        return false;
+    }
+
+}
