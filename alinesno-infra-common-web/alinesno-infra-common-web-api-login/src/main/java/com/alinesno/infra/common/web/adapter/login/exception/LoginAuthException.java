4c7f36291127b0120cb9313388b266c27f11d9aa
陈伟(Victor) <victorAip@gmail.com>
2024-10-20 13:22:55 +0800

    feat: 添加登陆认证异常类 `LoginAuthException`

---
+++ alinesno-infra-common-web/alinesno-infra-common-web-api-login/src/main/java/com/alinesno/infra/common/web/adapter/login/exception/LoginAuthException.java
@@ -0,0 +1,12 @@
+package com.alinesno.infra.common.web.adapter.login.exception;
+
+/**
+ * 登陆认证异常
+ */
+public class LoginAuthException extends RuntimeException {
+
+    public LoginAuthException(Throwable cause) {
+        super(cause);
+    }
+
+}
