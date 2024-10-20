0aff9fa99ec0d3e6ed912e7f18762fd6d3736506
周健(James) <jamesAip@gmail.com>
2024-10-21 00:10:20 +0800

    feat:在 SecurityProperties 类中添加 @Primary 注解

--- alinesno-infra-common-web/alinesno-infra-common-web-api-login/src/main/java/com/alinesno/infra/common/web/adapter/login/security/SecurityProperties.java
+++ alinesno-infra-common-web/alinesno-infra-common-web-api-login/src/main/java/com/alinesno/infra/common/web/adapter/login/security/SecurityProperties.java
@@ -2,12 +2,14 @@ package com.alinesno.infra.common.web.adapter.login.security;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Primary;
 
 /**
  * Security 配置属性
  *
  * @author luoxiaodong
  */
+@Primary
 @Data
 @ConfigurationProperties(prefix = "alinesno.security")
 public class SecurityProperties {
