a8be89620ee3c6f4b1e587bca849adc43071c2f7
周健(James) <jamesAip@gmail.com>
2024-10-21 00:10:19 +0800

    feat: 添加 Meta 类，包含菜单相关的基本属性和构造方法。

---
+++ alinesno-infra-common-web/alinesno-infra-common-web-api-adapter/src/main/java/com/alinesno/infra/common/web/adapter/base/dto/Meta.java
@@ -0,0 +1,31 @@
+package com.alinesno.infra.common.web.adapter.base.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@Data
+public class Meta implements Serializable {
+    private String title; // 菜单标题
+    private String icon; // 菜单图标
+    private boolean noCache; // 是否不缓存
+    private String link; // 链接
+
+    /**
+     * 构造方法
+     * 创建一个具有基本属性的菜单元信息对象。
+     *
+     * @param title 菜单标题
+     * @param icon 菜单图标
+     * @param noCache 是否不缓存
+     * @param link 链接
+     */
+    public Meta(String title, String icon, boolean noCache, String link) {
+        this.title = title;
+        this.icon = icon;
+        this.noCache = noCache;
+        this.link = link;
+    }
+}
