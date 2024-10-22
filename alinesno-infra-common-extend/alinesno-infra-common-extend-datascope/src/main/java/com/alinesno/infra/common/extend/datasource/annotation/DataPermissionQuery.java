47b562a06a23bc22940018f8a2ada37056fbabb3
李娜(Allen) <allenAip@gmail.com>
2024-10-23 07:15:30 +0800

    feat: 添加数据权限查询注解 `DataPermissionQuery`，支持指定数据权限范围。

---
+++ alinesno-infra-common-extend/alinesno-infra-common-extend-datascope/src/main/java/com/alinesno/infra/common/extend/datasource/annotation/DataPermissionQuery.java
@@ -0,0 +1,21 @@
+package com.alinesno.infra.common.extend.datasource.annotation;
+
+
+import com.alinesno.infra.common.extend.datasource.enums.DataSourceScope;
+
+import java.lang.annotation.*;
+
+/**
+ * 数据权限查询拦截
+ */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface DataPermissionQuery {
+
+    /**
+     * 数据权限范围
+     */
+    DataSourceScope scope() default DataSourceScope.ORGANIZATIONAL_SCOPE  ;
+
+}
