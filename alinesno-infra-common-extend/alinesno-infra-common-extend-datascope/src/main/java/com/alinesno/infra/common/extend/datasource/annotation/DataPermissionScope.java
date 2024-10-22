9734d0c0e8a89df8d9362a6f86c46b85018b77c7
李娜(Allen) <allenAip@gmail.com>
2024-10-23 07:15:30 +0800

    feat:新增数据权限注解 `DataPermissionScope`，用于控制数据访问范围和自定义权限过滤。

---
+++ alinesno-infra-common-extend/alinesno-infra-common-extend-datascope/src/main/java/com/alinesno/infra/common/extend/datasource/annotation/DataPermissionScope.java
@@ -0,0 +1,27 @@
+package com.alinesno.infra.common.extend.datasource.annotation;
+
+import com.alinesno.infra.common.extend.datasource.enums.DataSourceScope;
+
+import java.lang.annotation.*;
+
+/**
+ * 数据权限拦截
+ */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface DataPermissionScope {
+
+    /**
+     * 自定义权限Bean过滤名称
+     *
+     * @return
+     */
+    public String roleBean() default "";
+
+    /**
+     * 数据权限范围
+     */
+    DataSourceScope scope() default DataSourceScope.ORGANIZATIONAL_SCOPE  ;
+
+}
