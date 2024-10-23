a0545a1f7acf611988ca27f95ec6f4859f53ddb3
王磊(Levi) <leviAip@gmail.com>
2024-10-23 23:12:51 +0800

    优化: 移除 CommonLoginController 中未使用的导入语句

--- alinesno-infra-common-web/alinesno-infra-common-web-api-login/src/main/java/com/alinesno/infra/common/web/adapter/login/controller/CommonLoginController.java
+++ alinesno-infra-common-web/alinesno-infra-common-web-api-login/src/main/java/com/alinesno/infra/common/web/adapter/login/controller/CommonLoginController.java
@@ -14,7 +14,6 @@ import com.alinesno.infra.common.web.adapter.login.account.CurrentAccountBean;
 import com.alinesno.infra.common.web.adapter.login.account.CurrentAccountJwt;
 import com.alinesno.infra.common.web.adapter.login.annotation.CurrentAccount;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,7 +21,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
