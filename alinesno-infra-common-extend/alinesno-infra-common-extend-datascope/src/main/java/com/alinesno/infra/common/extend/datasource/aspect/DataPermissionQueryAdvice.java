a8b3457c1a5bd97fa5643234dc3ea8ad2e1f01cf
李娜(Allen) <allenAip@gmail.com>
2024-10-23 19:20:15 +0800

    refactor: 修改数据权限查询切面的导入包路径。

--- alinesno-infra-common-extend/alinesno-infra-common-extend-datascope/src/main/java/com/alinesno/infra/common/extend/datasource/aspect/DataPermissionQueryAdvice.java
+++ alinesno-infra-common-extend/alinesno-infra-common-extend-datascope/src/main/java/com/alinesno/infra/common/extend/datasource/aspect/DataPermissionQueryAdvice.java
@@ -1,8 +1,8 @@
 package com.alinesno.infra.common.extend.datasource.aspect;
 
 import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionQuery;
-import com.alinesno.infra.common.extend.datasource.bean.PermissionQuery;
-import com.alinesno.infra.common.extend.datasource.enums.DataSourceScope;
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
+import com.alinesno.infra.common.facade.datascope.DataSourceScope;
 import com.alinesno.infra.common.web.adapter.login.account.CurrentAccountJwt;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
@@ -31,7 +31,7 @@ public class DataPermissionQueryAdvice {
 	private HttpServletRequest request;
 
 	// 拦截指定的方法,这里指只拦截TestService.getResultData这个方法
-	@Pointcut("@annotation(com.alinesno.infra.base.authority.annotation.DataPermissionQuery)")
+	@Pointcut("@annotation(com.alinesno.infra.common.extend.datasource.annotation.DataPermissionQuery)")
 	public void pointcut() {
 
 	}
