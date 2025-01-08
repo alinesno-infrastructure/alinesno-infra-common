2f01a6f13cd0eb90a781575adc4f82752a826f26
李伟(Michael) <michaelAip@gmail.com>
2025-01-08 19:47:29 +0800

    feat:新增 XssProperties 类，用于配置XSS过滤相关属性

---
+++ alinesno-infra-common-security/alinesno-infra-common-security-api/src/main/java/com/alinesno/infra/common/security/api/properties/XssProperties.java
@@ -0,0 +1,28 @@
+package com.alinesno.infra.common.security.api.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * xss过滤 配置属性
+ *
+ * @author Lion Li
+ */
+@Data
+@ConfigurationProperties(prefix = "alinesno.xss")
+public class XssProperties {
+
+    /**
+     * Xss开关
+     */
+    private Boolean enabled;
+
+    /**
+     * 排除路径
+     */
+    private List<String> excludeUrls = new ArrayList<>();
+
+}
